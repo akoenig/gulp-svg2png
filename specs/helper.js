cebfe29a6a06af6307b3a395f2720cf929a1958a
André König <andre.koenig@gmail.com>
2014-02-13 14:13:37 +0100

    Written unit test for checking the scale functionality.

--- specs/helper.js
+++ specs/helper.js
@@ -13,6 +13,12 @@
 
 'use strict';
 
+var fs        = require('fs'),
+    os        = require('os'),
+    path      = require('path'),
+    imagesize = require('imagesize'),
+    gutil     = require('gulp-util');
+
 /**
  * Checks if a given buffer is valid data from a PNG.
  *
@@ -27,4 +33,47 @@ exports.isPNG = function isPNG (buffer) {
     buffer = buffer.toString('hex').toUpperCase();
 
     return (buffer.substring(0, mnumber.length) === mnumber);
+};
+
+/**
+ * Checks if the dimensions of an image matches
+ * given dimensions.
+ *
+ * @param  {string} image Path to the image.
+ * @param  {number} width The expected width.
+ * @param  {number} height The expected height.
+ * @param  {function} callback
+ *
+ * @return {Boolean}
+ *
+ */
+exports.hasDimensions = function hasDimensions (image, height, width, callback) {
+    var temp = path.join(os.tmpdir(), 'test.png');
+
+    fs.writeFileSync(temp, image);
+
+    imagesize(fs.createReadStream(temp), function onread (err, result) {
+        if (err) {
+            return callback(err);
+        }
+
+        fs.unlinkSync(temp);
+
+        return callback(null, (result.width === width && result.height === height));
+    });
+};
+
+/**
+ * Creates a vinyl file descriptor for testing.
+ *
+ * @return {object}
+ *
+ */
+exports.createTestFile = function createTestFile () {
+    return new gutil.File({
+        cwd:  './specs/assets/',
+        base: './specs/assets/',
+        path: './specs/assets/twitter.svg',
+        contents: fs.readFileSync('./specs/assets/twitter.svg')
+    });
 };
--- specs/plugin.spec.js
+++ specs/plugin.spec.js
@@ -15,26 +15,38 @@
 
 var fs      = require('fs'),
     svg2png = require('../'),
-    helper  = require('./helper'),
-    gutil   = require('gulp-util');
+    helper  = require('./helper');
 
 describe('The "gulp-svg2png" plugin', function () {
 
     it('should convert a SVG to a PNG', function (done) {
         var filename = 'twitter.svg',
             stream   = svg2png(),
-            image    = new gutil.File({
-                cwd:  './specs/assets/',
-                base: './specs/assets/',
-                path: './specs/assets/twitter.svg',
-                contents: fs.readFileSync('./specs/assets/twitter.svg')
-            });
+            image    = helper.createTestFile();
 
         stream.on('data', function (png) {
             expect(png.path).toBe('twitter.png');
             expect(helper.isPNG(png.contents)).toBe(true);
 
             done();
+        });
+
+        stream.write(image);
+        stream.end();
+    });
+
+    it('should convert a SVG to a PNG by a defined scaling factor', function (done) {
+        var factor = 1.1,
+            filename = 'twitter.svg',
+            stream   = svg2png(factor),
+            image    = helper.createTestFile();
+
+        stream.on('data', function (png) {
+            helper.hasDimensions(png.contents, 191, 190, function (err, has) {
+                expect(has).toBe(true);
+
+                done();
+            });
         });
 
         stream.write(image);
